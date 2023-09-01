a300e0d01691cb2d4f567a52cc55d0c32d09a489
marvin9830 <asamieto965@gmail.com>
2023-09-01 22:53:42 +0100

    build: init base template structure

---
+++ static/base/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'primary':'#111111',
+          'secondary':'#566565',
+        },
+        container: {
+          center: true,
+          padding: '2rem',
+        },
+        fontFamily: {
+          sans: ['Roboto', 'sans-serif'],
+        },
+
+      }
+    },
+  }
