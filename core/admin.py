4b198985424a43177b4433d1e0c8c4b7f827b0ec
marvin9830 <asamieto965@gmail.com>
2023-09-04 17:47:10 +0100

    chore: modify product model admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Product, ProductCategory, ProductImage
 
-admin.site.register(Product)
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductImageInline,
+    ]
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductCategory)
 admin.site.register(ProductImage)
