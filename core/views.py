2a973dad0ec4e0e05e256c0f3e0aa03c588e7343
marvin9830 <asamieto965@gmail.com>
2023-09-04 21:15:31 +0100

    feat: add featured and offers to homepage

--- core/views.py
+++ core/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from .models import Product
+import random
+
 
 def home_view(request):
-    return render(request, 'home/home.html')
+    # SELECT ALL FEATURED PRODUCTS
+    featured_products = Product.objects.filter(is_featured=True)
+    # CONTROL MAX FEATURED PRODUCTS TO SHOW
+    num_featured_to_show = min(len(featured_products), 6)
+    featured_products = random.sample(list(featured_products), num_featured_to_show)
+
+    # SELECT ALL SALE PRODUCTS
+    sale_products = Product.objects.filter(sale_price__gt=0)
+    # CONTROL MAX SALE PRODUCTS TO SHOW
+    num_sale_to_show = min(len(sale_products), 6)
+    sale_products = random.sample(list(sale_products), num_sale_to_show)
+
+    context = {
+        "featured_products": featured_products,
+        "sale_products": sale_products,
+    }
+    return render(request, "home/home.html", context)
