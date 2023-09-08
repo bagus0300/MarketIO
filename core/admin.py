979e36e4820fdf12a6831e591554bae612d6c78e
marvin9830 <asamieto965@gmail.com>
2023-09-08 16:28:07 +0100

    feat: add ProductVariant model

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
-from .models import Product, ProductCategory, ProductImage
+from .models import *
+
 
 class ProductImageInline(admin.StackedInline):
     model = ProductImage
 
+
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+
+
 class ProductAdmin(admin.ModelAdmin):
     inlines = [
         ProductImageInline,
+        ProductVariantInline,
     ]
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductCategory)
 admin.site.register(ProductImage)
+admin.site.register(ProductVariant)
--- core/models.py
+++ core/models.py
@@ -57,10 +57,10 @@ class ProductCategory(models.Model):
 
 
 def upload_to_product_img(instance, filename):
-    """ 
+    """
     Defines the upload path for product images.
     """
-    
+
     filename = os.path.basename(filename)
     return os.path.join("product_imgs", filename)
 
@@ -73,4 +73,33 @@ class ProductImage(models.Model):
     image = models.ImageField(_("Image"), upload_to=upload_to_product_img)
     product = models.ForeignKey(
         "core.Product", on_delete=models.SET_NULL, null=True, blank=True, default=None
+    )
+
+
+class ProductVariant(models.Model):
+    """
+    Product variant model for storing product variants.
+    Defines the product size and quantity in stock.
+    """
+
+    SIZES = [
+        ("5.5", "UK 5.5"),
+        ("6", "UK 6"),
+        ("6.5", "UK 6.5"),
+        ("7", "UK 7"),
+        ("7.5", "UK 7.5"),
+        ("8", "UK 8"),
+        ("8.5", "UK 8.5"),
+        ("9", "UK 9"),
+        ("9.5", "UK 9.5"),
+        ("10", "UK 10"),
+        ("10.5", "UK 10.5"),
+        ("11", "UK 11"),
+        ("12.5", "UK 12.5"),
+        ("13", "UK 13"),
+    ]
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    size = models.CharField(_("Size"), max_length=50, choices=SIZES)
+    quantity = models.PositiveIntegerField(
+        _("Qty in stock"), blank=True, default=0, null=True
     )
